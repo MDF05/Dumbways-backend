e2bf31d62f4a7af92390974a5959a33f10fba9f9
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-07-30 13:42:29 +0700

    fix: list white cors

--- app.mjs
+++ app.mjs
@@ -13,7 +13,7 @@ app.use(
         origin: [
             "https://dumbways-nine.vercel.app",
             "http://127.0.0.1:5501",
-            "http://localhost:3000",
+            "http://127.0.0.1:3000",
         ],
     }),
 )
